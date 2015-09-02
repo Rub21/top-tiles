cc2b013339385c7b9dffd05cd8de3138c6689924
Rub21 <ruben@mapbox.com>
2015-09-02 11:34:55 -0500

    fix

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ var fs = require('fs');
 var tilebelt = require('tilebelt');
 var tilecover = require('tile-cover');
 var t = require('turf');
-var _ = require('underscore');
 var jsonfile = require('jsonfile');
 var csv2geojson = require('csv2geojson');
 jsonfile.readFile('japan.geojson', function(err, obj) {
 	var limits = {
-			min_zoom: 9,
-			max_zoom: 9
+			min_zoom: 13,
+			max_zoom: 13
 		}
-		// var tiles = tilecover.tiles(geojson.geometry, limits); //num tiles in geojson
-	var json_tiles = tilecover.geojson(obj.features[0].geometry, limits); //geojson from each tile in geojson
-	// fs.writeFile('outputo.geojson', JSON.stringify(json_tiles), function(err) {
-	// 	if (err)
-	// 		return console.log(err);
-	// 	console.log('resuotl');
-	// });
+	var json_tiles = tilecover.geojson(obj.features[0].geometry, limits); 
 	fs.readFile('twitter.csv', 'utf8', function(err, data) {
 		if (err) {
 			return console.log(err);
